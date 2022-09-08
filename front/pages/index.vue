e98f115cc09e27f32f8f9f758b51c411968700e9
Park taeseong <tspark9946@gmail.com>
2022-09-08 18:36:33 +0900

    vue proj init

---
+++ front/pages/index.vue
@@ -0,0 +1,17 @@
+<template>
+  <v-row>
+    <v-col>
+      <Form />
+    </v-col>
+    <v-col>
+      <Table />
+    </v-col>
+  </v-row>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style>
+</style>
